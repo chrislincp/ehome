86dc7a4c5bd841f6f61560457cfa3de7981d9e91
ChrisLin <chrislincp@163.com>
2017-08-22 14:25:32 +0800

    产品设置修改

--- src/views/system/config/products.vue
+++ src/views/system/config/products.vue
@@ -15,7 +15,7 @@
 </template>
 
 <script>
-import { mapGetters, mapActions  } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 
 export default {
   data () {
@@ -38,99 +38,108 @@ export default {
       'GetStructureData',
       'PostStructure'
     ]),
+    getRequest () {
+      const params = { type: 1, fc: 1, UserID: 42, Visibility: 1 }
+      this.GetStructureData(params)
+    },
+    postRequest (params) {
+      this.PostStructure(params).then(res => {
+        const { code, msg } = res
+        if (code == 200) {
+          this.$message({
+            type:'success',
+            message: msg
+          })
+          this.getRequest()
+        }else {
+          this.$message({
+            type: 'error',
+            message: msg
+          })
+        }
+
+      })
+    },
     renderContent (createElement, { node, data, store }) {
-                var that = this;
-                return createElement('span', [
-                    createElement('span', node.label),
-                    createElement('span', {attrs:{
-                        style:"float: right; margin-right: 10px"
-                    }},[
-                        createElement('el-button',{attrs:{
-                            style: 'margin: 0 10px',
-                            type: 'text',
-                            icon: 'edit'
-                        },on:{
-                            click:function() {
-                              that.$prompt('请输入名称', '提示', {
-                                confirmButtonText: '确定',
-                                cancelButtonText: '取消',
-                              }).then(({ value }) => {
-                                const para = { type: 0, fc: 4, ID: node.data.id, label: value }
-                                that.PostStructure(para).then(res => {
-                                  const { code, msg } = res
-                                  if (code == 200) {
-                                    that.$message({
-                                      type:'success',
-                                      message: msg
-                                    })
-                                  }
-                                  that.GetStructureData()
-                                })
-                              }).catch(() => {
+      var that = this;
+      const btnVisible = node.level < 3 ? '' : 'none'
+      return createElement('span', [
+          createElement('span', node.label),
+          createElement('span', {attrs:{
+              style:"float: right; margin-right: 10px"
+          }},[
+              createElement('el-button',{attrs:{
+                style: `margin: 0 10px;`,
+                type: 'text',
+                icon: 'edit'
+              },on:{
+                  click:function() {
+                    that.$prompt('请输入名称', '提示', {
+                      confirmButtonText: '确定',
+                      cancelButtonText: '取消',
+                    }).then(({ value }) => {
+                      const para = {
+                        type: 0,
+                        fc: 4,
+                        ID: node.data.id,
+                        label: value
+                      }
+                      that.postRequest(para)
+                    }).catch(() => {
 
-                              })
-                                // store.append({ id: that.baseId++, label: 'testtest', children: [] }, data);
-                            }
-                        }}),
-                        createElement('el-button',{attrs:{
-                          style: 'margin: 0 10px',
-                            type: 'text',
-                            icon: 'plus'
-                        },on:{
-                            click:function() {
-                              that.$prompt('请输入名称', '提示', {
-                                confirmButtonText: '确定',
-                                cancelButtonText: '取消',
-                              }).then(({ value }) => {
-                                // store.append({ id: that.baseId++, label: value, children: [] }, data);
-                                const para = { type: 0, fc: 2, UserID: 42, FID: node.data.id, label: value }
-                                that.PostStructure(para).then(res => {
-                                  const { code, msg } = res
-                                  if (code == 200) {
-                                    that.$message({
-                                      type:'success',
-                                      message: msg
-                                    })
-                                  }
-                                  that.GetStructureData()
-                                })
-                              }).catch(() => {
+                    })
+                  }
+              }}),
+              createElement('el-button',{attrs:{
+                style: `margin: 0 10px; display: ${btnVisible}`,
+                type: 'text',
+                icon: 'plus'
+              },on:{
+                  click:function() {
+                    that.$prompt('请输入名称', '提示', {
+                      confirmButtonText: '确定',
+                      cancelButtonText: '取消',
+                    }).then(({ value }) => {
+                      const para = {
+                        type: 0,
+                        fc: 2,
+                        UserID: 42,
+                        FID: node.data.id,
+                        label: value
+                      }
+                      that.postRequest(para)
+                    }).catch(() => {
 
-                              })
+                    })
 
-                            }
-                        }}),
-                        createElement('el-button',{attrs:{
-                          style: 'margin: 0 10px',
-                          type: 'text',
-                          icon: 'delete'
-                        },on:{
-                            click:function() {
-                                const para = { type: 0, fc: 3, ID: node.data.id}
-                                that.$confirm(`确认删除 ${node.data.label}？`, '提示', {
-                                  confirmButtonText: '确定',
-                                  cancelButtonText: '取消',
-                                  type: 'warning'
-                                }).then(() => {
-                                  that.PostStructure(para).then(res => {
-                                    const { code, msg } = res
-                                    if (code == 200) {
-                                      that.$message({
-                                        type: 'success',
-                                        message: msg
-                                      })
-                                    }
-                                    that.GetStructureData()
-                                  })
-                                }).catch(() => {})
-                            }
-                        }}),
-                    ]),
-                ]);
-            }
+                  }
+              }}),
+              createElement('el-button',{attrs:{
+                style: `margin: 0 10px;`,
+                type: 'text',
+                icon: 'delete'
+              },on:{
+                  click:function() {
+                      that.$confirm(`确认删除 ${node.data.label}？`, '提示', {
+                        confirmButtonText: '确定',
+                        cancelButtonText: '取消',
+                        type: 'warning'
+                      }).then(() => {
+                        const para = {
+                          type: 0,
+                          fc: 3,
+                          ID: node.data.id
+                        }
+                        that.postRequest(para)
+                      }).catch(() => {})
+                  }
+              }}),
+          ]),
+      ]);
+    }
   },
   mounted () {
-    this.GetStructureData()
+    this.getRequest()
   }
 }
 </script>
